bf9738acdf40c960690a76496c589808063c0d7e
Awais JMM <jmm.awaiskiyani@gmail.com>
2024-09-29 17:05:07 +0500

    Add GetItemMethod

--- DatabasePracticeWMS/Repository/ItemRepository.cs
+++ DatabasePracticeWMS/Repository/ItemRepository.cs
@@ -1,12 +1,28 @@
-﻿using DatabasePracticeWMS.Models;
+﻿using Dapper;
+using DatabasePracticeWMS.Models;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace DatabasePracticeWMS.Repository
 {
     public class ItemRepository
     {
+
+        private readonly IConfiguration _configuration;
+
+        public ItemRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
         public async Task<IEnumerable<GetItemDetails>> GetItemDetails()
         {
-
+            var connection = _configuration.GetConnectionString("DefaultConnection");
+            SqlConnection sqlConnection = new SqlConnection(connection);
+            var items = await sqlConnection.QueryAsync<GetItemDetails>("spGetUserByEmail",
+            commandType: CommandType.StoredProcedure).ConfigureAwait(false);
+            return items;
         }
     }
 }
